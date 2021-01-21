3229e3b8053ed4fb0e2242c99215dd544d8d944c
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2021-01-21 22:17:39 +0530

    created class phonebook

--- phone_book.cpp
+++ phone_book.cpp
@@ -5,14 +5,27 @@ class phonebook;
 
 class contact
 {
-    string name;
-    int number;
-    string email;
+    string contact_name;
+    int contact_number;
+    string contact_email;
+    contact *frow;
+    contact *prev;
     public:
     friend class phonebook;
 };
 
 class phonebook
 {
-    
-}
+    string name;
+    int number;
+    string email;
+    contact *start;
+    public:
+    phonebook()
+    {
+        start = NULL;
+    }
+    void add_contact();
+    void delete_contact();
+    void search();
+};
